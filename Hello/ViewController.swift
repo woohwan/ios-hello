b52c61ab128739056047cd7b079d29b3c7eead1e
whpark70 <whpark70@gmail.com>
2023-06-01 08:33:41 +0000

    first commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by ec2-user on 6/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
